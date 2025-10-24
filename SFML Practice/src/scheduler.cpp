b59bbaa6e620e08db7d77273ede42fce869eb0c6
iammiguelsolis <miguelsoliscunza24@gmail.com>
2025-10-23 23:59:15 -0500

    fix

---
+++ SFML Practice/src/scheduler.cpp
@@ -0,0 +1,47 @@
+#include <fstream>
+#include <filesystem>
+#include <iostream>
+
+#include "scheduler.h"
+#include "os_sim.h"
+
+// --- Default virtual behaviour ---
+
+void IScheduler::onProcessStart(os_sim::PCB& process) {
+    using os_sim::global::g_CPUBurstClock;
+    g_CPUBurstClock.restart();
+    process.cpuBurstClock.restart();
+#ifdef OS_DEBUG
+    std::cout << "\n [PROCESS " << process.pid << "]\tStarting execution.";
+#endif // !OS_DEBUG
+}
+
+void IScheduler::selectProcess(std::list<os_sim::PCB>& pcbQueue) {
+    using os_sim::global::g_isRunningProcess;
+    using os_sim::global::g_PCBCurrentlyRunning;
+
+    g_PCBCurrentlyRunning = pcbQueue.front();
+    pcbQueue.pop_front();
+    g_PCBCurrentlyRunning.processState = os_sim::PCBState::Running;
+    //g_PCBCurrentlyRunning.cpuBurstClock.restart();
+
+    g_isRunningProcess = true;
+    if (g_PCBCurrentlyRunning.responseTime == sf::Time::Zero) g_PCBCurrentlyRunning.responseTime = g_PCBCurrentlyRunning.processClock.restart();	// Response Time
+}
+
+
+// --- Output Metrics to File ---
+
+void IScheduler::outputMetrics([[maybe_unused]] os_sim::PCB& process) {
+    std::ofstream outFile("data/results.csv", std::ios::app);
+    if (!outFile.is_open()) return;
+
+    outFile
+        << process.pid << ","
+        << process.totalExecutionTime.asSeconds() << ','
+        << process.waitingTime.asSeconds() << ','
+        << process.responseTime.asSeconds() << ','
+        << process.totalWaitingTime.asSeconds() << '\n';
+
+    outFile.close();
+}
---
+++ SFML Practice/src/sjf.cpp
@@ -0,0 +1,54 @@
+#include "sjf.h"
+
+#ifdef OS_DEBUG
+#include <iostream>
+#endif // !OS_DEBUG
+
+#include "os_sim.h"
+#include "rng.h"
+
+void SJFScheduler::onProcessExecution(os_sim::PCB& process, [[maybe_unused]] std::list<os_sim::PCB>& pcbQueue) {
+	using os_sim::global::g_CPUBurstClock;
+	using os_sim::global::g_pcbWaitLine;
+	using os_sim::global::g_isRunningProcess;
+	// -- Simulate process finalization
+	if (os_sim::ifEnd(process)) {
+		process.processState = os_sim::PCBState::Terminated;
+		process.totalExecutionTime = process.processClock.reset() + process.responseTime;								//	<-------- Turnaround
+		process.waitingTime = process.totalExecutionTime - process.totalBurstTime - process.totalWaitingTime;			//	<-------- Waiting Time
+
+#ifdef OS_DEBUG
+		std::cout << "\n [PROCESS " << process.pid << "]\tTerminated after " << process.totalExecutionTime.asSeconds() << "s.";
+#endif // !OS_DEBUG
+		g_isRunningProcess = false;
+	}
+	// -- Simulate process waiting
+	else if (os_sim::ifStartWaiting(process)) {
+		process.processState = os_sim::PCBState::Waiting;
+		sf::Time saveTime{ g_CPUBurstClock.restart() };
+		process.totalBurstTime += saveTime;
+		process.minWaitDuration_s = process.ioBurstSim.front();
+		process.cpuBurstSim.pop();
+		process.ioBurstSim.pop();
+
+		process.lastBurstTime_s = saveTime.asSeconds();
+		process.estimatedBurstTime_s = process.alpha * process.lastBurstTime_s + (1 - process.alpha) * process.estimatedBurstTime_s;
+
+#ifdef OS_DEBUG
+		std::cout << "\n [PROCESS " << process.pid << "]\tHalted, waiting for IO. [Estimated Burst Time of " << process.estimatedBurstTime_s <<  "s.]";
+#endif // !OS_DEBUG
+
+		process.ioBurstClock.restart();
+		g_pcbWaitLine.push_back(process);
+		g_isRunningProcess = false;
+	}
+}
+
+void SJFScheduler::returnToQueue(os_sim::PCB& process, std::list<os_sim::PCB>& pcbQueue) {
+	process.processState = os_sim::PCBState::Ready;
+	auto insertPos = std::find_if(pcbQueue.begin(), pcbQueue.end(),
+		[&process](const os_sim::PCB& other) {
+			return process.estimatedBurstTime_s < other.estimatedBurstTime_s;
+		});
+	pcbQueue.insert(insertPos, process);			// Insert elements already sorted so selectProcess() can just pick the front.
+}
